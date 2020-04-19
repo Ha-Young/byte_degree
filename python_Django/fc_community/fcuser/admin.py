92034d1fd985eca231b98a650ad8073948f7c46f
Ha0 <hayeong28@naver.com>
2020-04-20 01:53:41 +0900

    make Fcuser Model(Fcuser class) and register admin page (FcuserAdmin)

--- python_Django/fc_community/fcuser/admin.py
+++ python_Django/fc_community/fcuser/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Fcuser
 
 # Register your models here.
+
+# 관리자페이지에 Fcuser 등록하고 관리할 수 있게 하기.
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password', 'registered_dttm') # 리스트로 보이게 하기.
+
+admin.site.register(Fcuser, FcuserAdmin)
--- python_Django/fc_community/fcuser/models.py
+++ python_Django/fc_community/fcuser/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+# Fcuser Model 생성. verbose_name으로 관리자페이지에 한글나오게 하기.
 class Fcuser(models.Model):
-    username = models.CharField(max_length=64,
+    username = models.CharField(max_length=32,
                                 verbose_name='사용자명')
-    password = models.CharField(max_length=64,
+    password = models.CharField(max_length=32,
                                 verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now=True,
                                             verbose_name='등록시간')
     
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'fastcampus_fcuser'
-
+        # 관리자페이지에 한글명나오게하기
+        verbose_name = '패스트캠퍼스 사용자'
+        verbose_name_plural = '패스트캠퍼스 사용자'
