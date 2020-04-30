ff513cd519e755d8558a6cab48d545d435776a44
hychoi <hayeong28@naver.com>
2020-04-30 20:10:02 +0900

    board admin apply && template - db linked

--- python_Django/fc_community/board/admin.py
+++ python_Django/fc_community/board/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+
+# 관리자페이지에 Board 등록하고 관리할 수 있게 하기.
+
+
+class BoardAdmin(admin.ModelAdmin):
+    # 리스트로 보이게 하기.
+    list_display = ('title', 'contents', 'writer', 'registered_dttm')
+
+
+admin.site.register(Board, BoardAdmin)
--- python_Django/fc_community/board/models.py
+++ python_Django/fc_community/board/models.py
@@ -7,7 +7,7 @@ class Board(models.Model):
     title = models.CharField(max_length=128, verbose_name="제목")
     contents = models.TextField(verbose_name="내용")
     writer = models.ForeignKey("fcuser.Fcuser", verbose_name="작성자", on_delete=models.CASCADE)
-    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name="")
+    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name="등록일자")
     
 
     class Meta:
