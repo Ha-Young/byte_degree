68671249676cc1ced8a2c2d6aea0e858e66bb68d
hychoi <hayeong28@naver.com>
2020-05-03 18:13:18 +0900

    make tag model and admin && migrate

--- python_Django/fc_community/tag/admin.py
+++ python_Django/fc_community/tag/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Tag
 # Register your models here.
+
+
+class TagAdmin(admin.ModelAdmin):
+    # 리스트로 보이게 하기.
+    list_display = ('name', 'registered_dttm')
+
+
+admin.site.register(Tag, TagAdmin)
